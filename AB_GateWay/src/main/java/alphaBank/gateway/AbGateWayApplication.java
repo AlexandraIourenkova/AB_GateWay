4562ba54c9fc2d63c259c9c221d55d92dfa05f50
sgor9 <sgor9@LAPTOP-902O5T37.mshome.net>
2022-04-28 12:19:43 +0300

    FirstCommit

---
+++ AB_GateWay/src/main/java/alphaBank/gateway/AbGateWayApplication.java
@@ -0,0 +1,13 @@
+package alphaBank.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbGateWayApplication.class, args);
+	}
+
+}
---
+++ AB_GateWay/src/test/java/alphaBank/gateway/AbGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package alphaBank.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
